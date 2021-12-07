c79730e364918af0de0f501a36cc37456488df16
Ana Rafaela <71745698+AnaaRaffaela@users.noreply.github.com>
2021-12-06 22:16:30 -0300

    Add files via upload

---
+++ lib/Flutter15/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Flutter11/main.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: PrimeiraRota(),
+      ),
+    );
