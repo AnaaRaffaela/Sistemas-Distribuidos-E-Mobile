c8a95294ccdb19aeda504f5f642068142bfe9161
Ana Rafaela <71745698+AnaaRaffaela@users.noreply.github.com>
2021-12-06 22:19:41 -0300

    Add files via upload

---
+++ lib/Flutter13/roteamento/roteamento.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Flutter12/rota/rota.dart';
+
+class Roteamento extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => PrimeiraTela(),
+        '/segunda': (context) => SegundaTela(),
+        '/terceira': (context) => TerceiraTela(),
+        '/quarta': (context) => QuartaTela(),
+      },
+    );
+  }
+}
