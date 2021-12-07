b56726ec261413aefc6f1326755bfd675c0b67c1
Ana Rafaela <71745698+AnaaRaffaela@users.noreply.github.com>
2021-12-07 16:16:33 -0300

    Delete Praticas directory

--- Praticas/lib/Flutter12/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/Material.dart';
-
-import 'roteamento/roteamento.dart';
-
-void main() {
-  runApp(Roteamento());
-}
--- Praticas/lib/Flutter15/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Flutter11/main.dart';
-
-void main() => runApp(
-      MaterialApp(
-        home: PrimeiraRota(),
-      ),
-    );
